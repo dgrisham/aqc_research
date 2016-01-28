f6b121636865653acf42be4da27adca8ec2248c1
Matthew Berntson <mberntso@bb136-20.mines.edu>
2016-01-28 16:37:24 -0700

    Added linear programs

---
+++ programs/algorithms/linear/AQCSatSchedClassic.py
@@ -0,0 +1,199 @@
+# author: mberntso  last update: 1/23/15  
+
+from operator import sub
+
+# initialize all tasks as scheduled
+
+# numTasks = 5
+
+x = [1,1,1,1,1]
+
+# global indeces for first and last scheduled tasks
+
+firstIndex = 0
+
+lastIndex = len(x)-1
+
+# initialize start times and precedence matrix later
+
+# model inputs
+
+d = [1,2,1,1,1]
+
+# slew time in accordance with index ordering
+
+s = [[0 for z in range(5)] for z in range(5)]
+
+# s[i][j] = time from i to j
+
+s[0][1] = 1
+s[0][2] = 1
+s[0][3] = 1
+s[0][4] = 1
+s[1][2] = 1
+s[1][3] = 1
+s[1][4] = 1
+s[2][3] = 1
+s[2][4] = 1
+s[3][4] = 1
+
+# window begin and end times
+
+wB = [0,1,2,3,4]
+wE = [2,3,4,5,6]
+
+# priorities
+
+p = [1,2,2,2,2]
+
+# very large value (not used currently)
+
+M = 999
+
+# build y (precedence) matrix (not used currently, b/c ordering is assumed)
+
+def precedenceMatrix(x):
+    numTasks = len(x)
+
+    y = [[0 for z in range(numTasks)] for z in range(numTasks)]
+
+    # y[i][j] = 1 if i < j
+
+    # todo: for loops, when data from I/O
+
+    y[0][1] = 1
+    y[0][2] = 1
+    y[0][3] = 1
+    y[0][4] = 1
+    y[1][2] = 1
+    y[1][3] = 1
+    y[1][4] = 1
+    y[2][3] = 1
+    y[2][4] = 1
+    y[3][4] = 1 
+
+    return y
+
+# build bounds for starting time b
+
+bMin = wB
+bMax = map(sub, wE, d)
+
+# findNext/Prev scheduled task (index)
+
+def findNext(x, index, last):
+    if index >= last:
+       return last
+    if x[index+1] == 1:
+        return index+1
+    else:
+        return findNext(x, index+1, last)
+
+def findPrev(x, index, first):
+    if index <= first:
+        return first
+    if x[index-1] == 1:
+        return index-1
+    else:
+        return findPrev(x, index-1, first)
+
+# remove task at (index) and update bounds on indeces
+
+def removeTask(x, index, first, last, currentV):
+    x[index] = 0
+    currentV = currentV - p[index]
+    if index == first:
+        first = findNext(x, index, last)
+    if index == last:
+        last = findPrev(x, index, first)
+    return (x,first,last,currentV)
+
+# intialize the objective function V as if all tasks are scheduled
+
+currentV = sum(p)
+
+
+# recursively determine the optimal schedule
+
+#
+# Perhaps following an approach in line with the additive algorithm
+# would clean up the mess of conditionals in this function
+#
+# See Figure 1 and 1a in the Geoffrion paper for a similar algorithm.
+# The Geoffrion paper also includes some explanation of a nonlinear
+# objective function for binary free variables.  That seems like a plausible
+# extension to what is here.
+#
+
+def optimalValue(x, index, first, last, currentV, bMin, bMax):
+    while index <= last: # O(n)
+        next = findNext(x, index, last) # O(n)
+        if index == last:
+            if bMin[index] > bMax[index]:
+                (x,first,last,currentV) = removeTask(x, index, first, last, currentV)
+            break
+        if bMin[index] > bMax[index]:
+            (x,first,last,currentV) = removeTask(x, index, first, last, currentV)
+        else:
+            if bMin[index]+d[index]+s[index][next] > bMax[next]:
+
+                # removal and comparison
+
+                bMin_Z = list(bMin)
+                bMax_Z = list(bMax)
+
+                (x_A,first_A,last_A,currentV_A) = removeTask(list(x), index, first, last, currentV)
+                index_A = findPrev(x, index, first)
+                (x_B,first_B,last_B,currentV_B,bMin_B,bMax_B) = optimalValue(x_A, index_A, first_A, last_A, currentV_A, bMin_Z, bMax_Z) # recursive => O(n^2)
+
+                (x_C,first_C,last_C,currentV_C) = removeTask(list(x), next, first, last, currentV)
+                index_C = findNext(x, index, last)
+                (x_D,first_D,last_D,currentV_D,bMin_D,bMax_D) = optimalValue(x_C, index_C, first_C, last_C, currentV_C, bMin_Z, bMax_Z)
+                
+                if currentV_B >= currentV_D:
+                    if currentV_B == currentV_D:
+                        if p[index] >= p[next]:        
+                            (x,first,last,currentV, bMin, bMax) = (x_B,first_B,last_B,currentV_B,bMin_B,bMax_B)
+                            index = findPrev(x, index, first)
+                        else:
+                            (x,first,last,currentV, bMin, bMax) = (x_D,first_D,last_D,currentV_D,bMin_D,bMax_D)
+                    else:
+                        (x,first,last,currentV, bMin, bMax) = (x_B,first_B,last_B,currentV_B,bMin_B,bMax_B)
+                        index = findPrev(x, index, first)
+                else:    
+                    (x,first,last,currentV, bMin, bMax) = (x_D,first_D,last_D,currentV_D,bMin_D,bMax_D)
+
+            else:
+
+                # adjust bounds if infeasible overlap
+
+                if bMin[index]+d[index]+s[index][next] <= bMin[next]:
+                    if bMax[index]+d[index]+s[index][next] > bMin[next]:
+                        if bMax[index]+d[index]+s[index][next] > bMax[next]:
+                            bMax[index] = bMax[next]-(d[index]+s[index][next])
+                else:
+                    bMin[next] = bMin[index]+(d[index]+s[index][next])
+
+        index = findNext(x, index, last)
+
+    return (x,first,last,currentV, bMin, bMax)
+
+
+# print the results
+
+(sched, blah, bleh, objFunc, startMin, startMax) = optimalValue(x, firstIndex, firstIndex, lastIndex, currentV, bMin, bMax)
+
+print('Scheduled Tasks: ')
+print(sched)
+print('Objective Function Value: ')
+print(sum([a*b for a,b in zip(sched,p)]))
+print('Start time min bound: ')
+print(startMin)
+print('Start time max bound: ')
+print(startMax)
+
+
+
+
+
+    
---
+++ programs/algorithms/linear/LawyerBinary.py
@@ -0,0 +1,155 @@
+# author: mberntso  last update: 1/23/15  
+
+class Cell:
+    def __init__(self, assigned, lawyer, case, time):
+        self.assigned = assigned
+        self.lawyer = lawyer
+        self.case = case
+        self.time = time
+        return
+
+
+def organizeData(data):
+
+    table = [[Cell(0,i,j,data[i][j]) for j in range(len(data[0]))] for i in range(len(data))]
+
+    # data table and lists by lawyer created
+
+    # now create sorted list of times in ascending order for additive algorithm
+
+    sortedList = []
+
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            sortedList.append(table[i][j])
+    
+    sortedList = sorted(sortedList, key=lambda Cell: Cell.time)
+            
+    #for i in range(len(sortedList)):
+    #    print(sortedList[i].time)
+
+    return table, sortedList
+
+
+def updateIncumbent(table, alloc, incumbent):
+
+    # check if updated allocation that satisfies constraints gives better obj func value
+
+    newValue = 0
+
+    for i in range(len(alloc)):
+        newValue += table[i][alloc[i]].time
+
+    print(newValue)
+
+    if newValue < incumbent:
+        incumbent = newValue
+
+    return incumbent
+
+
+def updateAlloc(table):
+
+    # update alloc from cells in table
+
+    alloc = [-1]*len(timeData)
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            if table[i][j].assigned == 1:
+                alloc[i] = j
+
+    print(str(alloc) + 'alloc')
+
+    return alloc
+
+# recursively determine the optimal allocation of cases
+# using Balas additive algorithm, assume root node cannot be solution
+
+def optimalAlloc(table, sortList, alloc, incumbent, index):
+    
+    # check constraints (one case per lawyer)
+
+    sumList = []
+
+    for i in range(len(table)):
+        tempSum = 0
+        for j in range(len(table[0])):
+            tempSum += table[i][j].assigned
+
+        if tempSum > 1:  # lawyer assigned to an impossible number of cases
+            print('impossible')
+            return (alloc, incumbent)
+
+        sumList.append(tempSum)
+
+    print(str(sumList)+'list')
+    for i in sumList:
+        # Check for infeasible constraint
+
+        if i == 0:
+            # Depth First Recursion
+            table[sortList[index].lawyer][sortList[index].case].assigned = 1
+            alloc = updateAlloc(table)
+            print('A')
+            (allocA, incumbentA) = optimalAlloc(table, sortList, alloc, incumbent, index+1)
+            print(allocA, incumbentA)
+
+            table[sortList[index].lawyer][sortList[index].case].assigned = 0
+            table[sortList[index+1].lawyer][sortList[index+1].case].assigned = 1
+            alloc = updateAlloc(table)
+            print('B')
+            (allocB, incumbentB) = optimalAlloc(table, sortList, alloc, incumbent, index+1)
+            print(allocB, incumbentB)
+
+            if incumbentB < incumbentA:
+                return (allocB, incumbentB)
+            return (allocA, incumbentA)
+
+    # satisfied constraints
+    alloc = updateAlloc(table)
+    incumbent = updateIncumbent(table, alloc, incumbent)
+
+    return (alloc, incumbent)
+
+
+# Results:
+
+# Setup data:
+
+# table of time data for each case (col) by each lawyer (row) => data[row][col]
+
+timeData = [[1., 2., 3.],
+        [4., 5., 6.],
+        [7., 8., 9.]]
+
+
+
+
+#[[145., 122., 130., 95., 115.],
+ #       [80., 63., 85., 48., 78.],
+  #      [121., 107., 93., 69., 95.],
+   #     [118., 83., 116., 80., 105.],
+    #    [97., 75., 120., 80., 111.]]
+
+cellTable, timeList = organizeData(timeData)
+
+# intialize the objective function V (to be minimized) to a high value
+# longest case time multiplied by number of cases >= possible function value
+
+value = timeList[-1].time * len(cellTable[0])
+
+# initialize null alloc
+# the index of alloc corresponds to a lawyer
+# the value at that index is the case assigned to that lawyer
+
+allocation = [-1]*len(timeData)
+
+# start algo at index = 0
+
+(allocation, value) = optimalAlloc(cellTable, timeList, allocation, value, 0)
+
+print('Case number by lawyer index: ')
+print(allocation)
+print('Objective Function Value: ')
+print(value)
+
