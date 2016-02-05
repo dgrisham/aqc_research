61e4d65026eb56ef4144a712976f8bb8eea4ce59
dgrisham <dgrisham@mines.edu>
2016-02-05 16:55:03 -0700

    made a few changes to aqc scheduler

--- programs/algorithms/linear/AQCSatSchedClassic.py
+++ programs/algorithms/linear/AQCSatSchedClassic.py
@@ -26,16 +26,9 @@ s = [[0 for z in range(5)] for z in range(5)]
 
 # s[i][j] = time from i to j
 
-s[0][1] = 1
-s[0][2] = 1
-s[0][3] = 1
-s[0][4] = 1
-s[1][2] = 1
-s[1][3] = 1
-s[1][4] = 1
-s[2][3] = 1
-s[2][4] = 1
-s[3][4] = 1
+for i in range(len(s)-1):
+    for j in range(i+1, len(s)):
+        s[i][j] = 1
 
 # window begin and end times
 
@@ -61,16 +54,9 @@ def precedenceMatrix(x):
 
     # todo: for loops, when data from I/O
 
-    y[0][1] = 1
-    y[0][2] = 1
-    y[0][3] = 1
-    y[0][4] = 1
-    y[1][2] = 1
-    y[1][3] = 1
-    y[1][4] = 1
-    y[2][3] = 1
-    y[2][4] = 1
-    y[3][4] = 1 
+    for i in range(len(y)-1):
+        for j in range(i+1, len(y)):
+            y[i][j] = 1
 
     return y
 
@@ -84,7 +70,7 @@ bMax = map(sub, wE, d)
 def findNext(x, index, last):
     if index >= last:
        return last
-    if x[index+1] == 1:
+    elif x[index+1] == 1:
         return index+1
     else:
         return findNext(x, index+1, last)
@@ -92,7 +78,7 @@ def findNext(x, index, last):
 def findPrev(x, index, first):
     if index <= first:
         return first
-    if x[index-1] == 1:
+    elif x[index-1] == 1:
         return index-1
     else:
         return findPrev(x, index-1, first)
@@ -144,11 +130,15 @@ def optimalValue(x, index, first, last, currentV, bMin, bMax):
 
                 (x_A,first_A,last_A,currentV_A) = removeTask(list(x), index, first, last, currentV)
                 index_A = findPrev(x, index, first)
-                (x_B,first_B,last_B,currentV_B,bMin_B,bMax_B) = optimalValue(x_A, index_A, first_A, last_A, currentV_A, bMin_Z, bMax_Z) # recursive => O(n^2)
+                (x_B,first_B,last_B,currentV_B,bMin_B,bMax_B) =\
+                optimalValue(x_A, index_A, first_A, last_A, currentV_A, bMin_Z,
+                        bMax_Z) # recursive => O(n^2)
 
                 (x_C,first_C,last_C,currentV_C) = removeTask(list(x), next, first, last, currentV)
                 index_C = findNext(x, index, last)
-                (x_D,first_D,last_D,currentV_D,bMin_D,bMax_D) = optimalValue(x_C, index_C, first_C, last_C, currentV_C, bMin_Z, bMax_Z)
+                (x_D,first_D,last_D,currentV_D,bMin_D,bMax_D) =\
+                optimalValue(x_C, index_C, first_C, last_C, currentV_C, bMin_Z,
+                        bMax_Z)
                 
                 if currentV_B >= currentV_D:
                     if currentV_B == currentV_D:
@@ -192,8 +182,3 @@ print(startMin)
 print('Start time max bound: ')
 print(startMax)
 
-
-
-
-
-    
